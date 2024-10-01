2151c08b8c7528bee0316fa8ba609d5072688d25
Ryua0105 <0101jcj@gmail.com>
2024-10-01 17:57:12 +0900

    Add Fuctions

--- sum_and_diff.py
+++ sum_and_diff.py
@@ -1,12 +1,18 @@
 Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> num1 = int(input("첫 번째 정수: "))
+>>> num = int(input("첫 번째 정수: "))
 첫 번째 정수: 10
 >>> num2 = int(input("두 번째 정수: "))
 두 번째 정수: 5
->>> sum = num1 + num2
->>> diff = num1 - num2
+>>> sum = num + num2
+>>> diff = num - num2
+>>> mul = num * num2
+>>> div = num / num2
 >>> print("두 수의 합: ", sum)
 두 수의 합:  15
 >>> print("두 수의 차: ", diff)
 두 수의 차:  5
+>>> print("두 수의 곱: ", mul)
+두 수의 곱:  50
+>>> print("두 수의 몫: ", div)
+두 수의 몫:  2.0
